822c7a06698f9f4597a58b99d74cd06775c95f94
1320503154 <1320503154@qq.com>
2025-03-06 23:19:33 +0800

    提交文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import ReactPlayground from "./ReactPlayground/index";
 import "./App.scss";
 import { PlaygroundProvider } from "./ReactPlayground/PlaygroundContext";
+import { ThemeProvider } from "./ReactPlayground/ThemeContext";
+
 function App() {
 	return (
-		<PlaygroundProvider>
-			<ReactPlayground />
-		</PlaygroundProvider>
+		<ThemeProvider>
+			<PlaygroundProvider>
+				<ReactPlayground />
+			</PlaygroundProvider>
+		</ThemeProvider>
 	);
 }
 
